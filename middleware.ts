01548d6f1a8adf57f29e8f0b3599cd92cf952ec3
Zhaomian Zhao <loisisar@outlook.com>
2023-07-07 10:16:57 +0100

    repro

--- middleware.ts
+++ middleware.ts
@@ -14,9 +14,27 @@ export default authMiddleware({
     "/privacy(.*)",
     "/pricing(.*)",
     "/api(.*)",
-   
+    // "/roadmap", // Public page but user needs to sign in to vote + list a feature
   ],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      // Don't do anything for public routes
+      return NextResponse.next();
+    }
 
+    const url = new URL(req.nextUrl.origin);
+    //const parts = req.nextUrl.pathname.split("/").filter(Boolean);
+
+    if (!auth.userId) {
+      // User is not signed in
+      url.pathname = "/sign-in";
+      return NextResponse.redirect(url);
+    }
+
+
+    return NextResponse.next();
+  },
+  // TODO: ComCord route will be changed to [projectName] once we are able to query organisation + user name
 })
 
 export const config = {
