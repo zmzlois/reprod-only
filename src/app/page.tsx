ec504680bd42ed89cd23fde9057cde0938185cd5
Lois <loisisar@outlook.com>
2023-11-10 16:49:42 +0000

    clear previous image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import ShowHint from "@/preserving-resetting-state/disappearing-input/page";
 import SwapTwoFields from "@/preserving-resetting-state/swap-two-form/page";
 import ContactManager from "@/preserving-resetting-state/reset-defailt-form/page";
-
+import Gallery from "@/preserving-resetting-state/clear-image/page";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
 
-<ContactManager/>
+<Gallery/>
 
     </main>
   )
