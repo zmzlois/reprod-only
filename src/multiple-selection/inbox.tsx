0aabaa36ee236df6009859d23782304d3f47edad
Lois <loisisar@outlook.com>
2023-11-10 12:22:51 +0000

    multiselection letter done

--- src/multiple-selection/inbox.tsx
+++ src/multiple-selection/inbox.tsx
@@ -4,14 +4,22 @@ import { letters } from './data';
 import Letter from './Letter';
 
 export default function MailClient() {
-    const [selectedId, setSelectedId] = useState(null);
+    const [selectedId, setSelectedId] = useState<number[]>([]);
+   // const [toggle, setToggle] = useState(false);
 
     // TODO: allow multiple selection
-    const selectedCount = 1;
+    const selected = (toggledId) =>  selectedId.find(id => id === toggledId);
 
     function handleToggle(toggledId) {
         // TODO: allow multiple selection
-        setSelectedId(toggledId);
+       if (selectedId.includes(toggledId)) {
+           setSelectedId(selectedId.filter(id => id !== toggledId))
+       } else {
+           setSelectedId([...selectedId, toggledId])
+       }
+
+
+
     }
 
     return (
@@ -24,15 +32,17 @@ export default function MailClient() {
                         letter={letter}
                         isSelected={
                             // TODO: allow multiple selection
-                            letter.id === selectedId
+                          selected(letter)
                         }
-                        onToggle={handleToggle}
+                        onToggle={(id) => {
+                            handleToggle(id)
+                        }}
                     />
                 ))}
                 <hr />
                 <p>
                     <b>
-                        You selected {selectedCount} letters
+                        You selected {selectedId.length} letters
                     </b>
                 </p>
             </ul>
