88a4a3db40148d7808e643223b0fe01923f7d9f7
Lois <loisisar@outlook.com>
2023-11-10 21:42:46 +0000

    scaling with reducer and context init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import ShowHint from "@/preserving-resetting-state/disappearing-input/page";
-import SwapTwoFields from "@/preserving-resetting-state/swap-two-form/page";
-import ContactManager from "@/preserving-resetting-state/reset-defailt-form/page";
-import Gallery from "@/preserving-resetting-state/clear-image/page";
-import LargeImage from "@/passing-data-context/page";
+import TaskApp from "@/reducer-context/page";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
 
-<LargeImage/>
-
+<TaskApp/>
     </main>
   )
 }
